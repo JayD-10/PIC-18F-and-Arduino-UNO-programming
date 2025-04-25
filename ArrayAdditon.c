b0e6d3511f8fcf4a36905996c4afaf12a91debd7
Jay Sandeep Darvekar <139309444+JayD-10@users.noreply.github.com>
2025-04-25 12:43:13 +0530

    Add files via upload

---
+++ ArrayAdditon.c
@@ -0,0 +1,15 @@
+#include<xc.h>
+#include<PIC18F4550.h>
+
+void main(void)
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int i,sum=0;
+    for(i=0;i<=9;i++)
+    {
+        sum=sum + arr[i];
+    }
+    TRISB = 0;
+    PORTB = sum;
+
+}  
