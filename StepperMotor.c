dac7640e6baf14e485d26d6f1d3807e6661b1bbd
Jay Sandeep Darvekar <darvekarjay@gmail.com>
2025-04-26 22:00:34 +0530

    Add files via upload

---
+++ StepperMotor.c
@@ -0,0 +1,30 @@
+#include <xc.h>
+#include <PIC18F4550.h>
+
+
+void delay(unsigned int time)
+{
+    unsigned int i,j;
+    for(i=0;i<time;i++)
+        for(j=0;j<5000;j++);
+}
+
+void main(void)
+{
+   TRISC = 0x00;
+   LATC = 0xFF;
+
+   while(1)                                //Loop forever;
+   {
+       LATC =  0x01;
+       delay(10);
+       LATC =  0x02;
+       delay(10);
+       LATC =  0x04;
+       delay(10);
+       LATC =  0x20;
+       delay(10);
+   }
+}
+
+
